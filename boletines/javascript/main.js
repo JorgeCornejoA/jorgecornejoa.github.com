288e669649aca96c5ae2f323224556372586dade
Coque <jorgecornejoa10@gmail.com>
2021-08-26 16:56:04 -0700

    Se agregó trabajo prueba

---
+++ boletines/javascript/main.js
@@ -0,0 +1,32 @@
+
+// Obtener los elementos de la clase .close
+
+let cerrar = document.querySelectorAll(".close")
+
+
+// Recorrerlos
+
+cerrar.forEach(function(close){
+
+    //Agregar un evento click a cada uno de ellos
+
+    close.addEventListener('click', function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content')
+        
+        //Quitarle las clases de animación que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases paraa animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href = "/";
+        }, 600);
+
+        return false
+    });
+});
+
